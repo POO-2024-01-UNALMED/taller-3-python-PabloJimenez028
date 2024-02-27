f50c5bfad7e500adb555525bcf8a19279f7d3ff0
PabloJimenez028 <juajimenezve@unal.edu.co>
2024-02-26 20:30:29 -0500

    Update tv.py

---
+++ televisores/tv.py
@@ -0,0 +1,81 @@
+from televisores.marca import Marca
+from televisores.control import Control
+
+class TV:
+    
+    numTV = 0
+
+    def __init__(self, marca:Marca, canal, precio, estado, volumen, control:Control):
+        self._marca = marca 
+        self._canal = 1
+        self._precio = 500
+        self._estado = estado
+        self._volumen = 1
+        self._control = control
+        self.numTV += 1
+
+    def TV(self, marca:Marca, estado:bool):
+        self._marca = marca
+        self._estado = estado
+
+    def setMarca(self, marca):
+        self._marca = marca
+
+    def getMarca(self):
+        return self._marca
+    
+    def setCanal(self, canal):
+        self._canal = canal
+
+    def getCanal(self):
+        return self._canal
+    
+    def setPrecio(self, precio):
+        self._precio = precio
+
+    def getPrecio(self, precio):
+        return self._precio
+    
+    def setVolumen(self, volumen):
+        self._volumen = volumen
+
+    def getVolumen(self):
+        return self._volumen
+    
+    def setControl(self, control):
+        self._control = control
+
+    def getControl(self):
+        return self._control
+    
+    def setNumTV(self, numero):
+        self.numTV = numero
+
+    def getNumTV(self):
+        return self.numTV
+
+    def turnOn(self):
+        self._estado = True
+
+    def turnOff(self):
+        self._estado = False
+    
+    def getEstado(self):
+        return self._estado
+    
+    def canalUp(self):
+        if self._canal >= 1 and self._canal < 120 and self._estado == True:
+            self._canal += 1
+
+    def canalDown(self):
+        if self._canal > 1 and self._canal <= 120 and self._estado == True:
+            self._canal -= 1
+
+    def volumenUp(self):
+        if self._volumen >= 0 and self._volumen < 7 and self._estado == True:
+            self._volumen += 1
+
+    def volumenDown(self):
+        if self._volumen >= 0 and self._volumen < 7 and self._estado == True:
+            self._volumen -= 1
+
